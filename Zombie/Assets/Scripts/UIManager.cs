96c534f7b24bcb226cd33e13bf68258e2850c52d
HongKyoungIn <ghdruddls999@naver.com>
2023-07-27 22:05:56 +0900

    18장 네트워크 이론과 로비 구현

--- Zombie/Assets/Scripts/UIManager.cs
+++ Zombie/Assets/Scripts/UIManager.cs
@@ -5,12 +5,9 @@ using UnityEngine.UI; // UI 관련 코드
 // 필요한 UI에 즉시 접근하고 변경할 수 있도록 허용하는 UI 매니저
 public class UIManager : MonoBehaviour {
     // 싱글톤 접근용 프로퍼티
-    public static UIManager instance
-    {
-        get
-        {
-            if (m_instance == null)
-            {
+    public static UIManager instance {
+        get {
+            if (m_instance == null) {
                 m_instance = FindObjectOfType<UIManager>();
             }
 
